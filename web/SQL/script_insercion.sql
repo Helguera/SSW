20ad49b0b469f48f15d1cc65d7d4ee738cf60cb8
helguera <javier.helguera@alumnos.uva.es>
2018-04-22 12:27:54 +0200

    Productos de ejemplo

--- web/SQL/script_insercion.sql
+++ web/SQL/script_insercion.sql
@@ -10,13 +10,21 @@ insert into usuario values ('Fernando','Zamora','12345698A','adios@adios.com','c
 
 -- INSERTAR PRODUCTOS 
 
-insert into productos values (1, 250);
-insert into productos values (2, 500);
-insert into productos values (3, 750);
+insert into productos values (1, 500);
+insert into productos values (2, 999);
+insert into productos values (3, 1690);
+insert into productos values (4, 19);
+insert into productos values (5, 20);
+insert into productos values (6, 22);
 
     -- INSERTAR IMPRESORAS
 
-    insert into impresoras3d values ();
+    insert into impresoras3d values (1, 'BQ', 'Hephestos', '(x)460 x (y)383 x (z)430 mm');
+    insert into impresoras3d values (2, 'BQ', 'Hephestos 2', '(x)450 x (y)516 x (z)571 mm');
+    insert into impresoras3d values (3, 'BQ', 'Witbox 2', '(x)508,06 x (y)485,08 x (z)461 mm');
        
     -- INSERTAR CONSUMIBLES
 
+    insert into consumibles values (4, 'PLA - Blanco', 'SmartMaterials');
+    insert into consumibles values (5, 'PLA - Azul', 'BQ');
+    insert into consumibles values (6, 'ABS - Verde', 'FormFutura');
