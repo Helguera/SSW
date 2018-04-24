354d30a1c7ce25e66196cbce5ed1d15e31a8b247
helguera <javier.helguera@alumnos.uva.es>
2018-04-24 18:40:05 +0200

    impresora3d

--- src/java/roles/Impresora3d.java
+++ src/java/roles/Impresora3d.java
@@ -14,5 +14,36 @@ public class Impresora3d extends Producto{
     private String modelo;
     private String tamaño;
     
+    public Impresora3d(){
+        super();
+    }
+    
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    
+    public void setModelo(String modelo){
+        this.modelo=modelo;
+    }
+    
+    public void setTamaño(String tamaño){
+        this.tamaño = tamaño;
+    }
+    
+    public String getMarca(){
+        return marca;
+    }
+    
+    public String getModelo(){
+        return modelo;
+    }
+    
+    public String getTamaño(){
+        return tamaño;
+    }
+   
+    
+    
+    
     
 }
