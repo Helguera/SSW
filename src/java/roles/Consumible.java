727f51b724215af9a1e10fedc326d8da1be3e877
josealbertodelval <josealbertodelval@192.168.0.154>
2018-04-24 18:38:38 +0200

    consumible

--- src/java/roles/Consumible.java
+++ src/java/roles/Consumible.java
@@ -10,5 +10,28 @@ package roles;
  * @author javier
  */
 public class Consumible extends Producto {
+    private String nombre;
+    private String marca;
     
+    public Consumible(){
+        super();
+    }
+    
+    
+    public void setNombre(String n){
+        this.nombre=n;
+    }
+    public void setMarca(String m){
+        this.marca=m;
+    }
+   
+
+    //Getters
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getMarca(){
+        return this.marca;
+    }
 }
